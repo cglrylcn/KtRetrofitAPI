c998a197caf21469971d5204396d5a604c7d23dd
Çağlar <caglar.yalcin@isuzu.com.tr>
2022-03-11 09:38:19 +0300

    add reactivex

--- app/src/main/java/com/caglar/ktretrofitapi/service/CryptoAPI.kt
+++ app/src/main/java/com/caglar/ktretrofitapi/service/CryptoAPI.kt
@@ -1,11 +1,13 @@
 package com.caglar.ktretrofitapi.service
 
 import com.caglar.ktretrofitapi.model.CryptoModel
+import io.reactivex.Observable
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface CryptoAPI {
     //https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json
     @GET("atilsamancioglu/K21-JSONDataSet/master/crypto.json")
-    fun getData(): Call<List<CryptoModel>>
+    fun getData(): Observable<List<CryptoModel>>
+//    fun getData(): Call<List<CryptoModel>>
 }
--- app/src/main/java/com/caglar/ktretrofitapi/view/MainActivity.kt
+++ app/src/main/java/com/caglar/ktretrofitapi/view/MainActivity.kt
@@ -9,7 +9,11 @@ import com.caglar.ktretrofitapi.adapter.RecyclerViewAdapter
 import com.caglar.ktretrofitapi.databinding.ActivityMainBinding
 import com.caglar.ktretrofitapi.model.CryptoModel
 import com.caglar.ktretrofitapi.service.CryptoAPI
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import retrofit2.*
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity(), RecyclerViewAdapter.Listener {
@@ -17,12 +21,13 @@ class MainActivity : AppCompatActivity(), RecyclerViewAdapter.Listener {
     private val BASE_URL = "https://raw.githubusercontent.com/"
     private var cryptoModels: ArrayList<CryptoModel> ?= null
     private var recyclerViewAdapter: RecyclerViewAdapter ?= null
+    private var compositeDisposable: CompositeDisposable ?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        compositeDisposable = CompositeDisposable()
         //RecyclerView
         val layoutManager: RecyclerView.LayoutManager = LinearLayoutManager(this)
         binding.recyclerView.layoutManager = layoutManager
@@ -34,36 +39,55 @@ class MainActivity : AppCompatActivity(), RecyclerViewAdapter.Listener {
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
-            .build()
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build().create(CryptoAPI::class.java)
 
-        val service = retrofit.create(CryptoAPI::class.java)
-        val call = service.getData()
-        call.enqueue(object : Callback<List<CryptoModel>> {
-            override fun onResponse(
-                call: Call<List<CryptoModel>>,
-                response: Response<List<CryptoModel>>
-            ) {
-                if (response.isSuccessful) {
-                    response.body()?.let { lCM ->
-                        recyclerViewAdapter = RecyclerViewAdapter(lCM as ArrayList<CryptoModel>, this@MainActivity)
-                        binding.recyclerView.adapter = recyclerViewAdapter
+        compositeDisposable?.add(retrofit.getData()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(this::handleResponse))
 
-                        cryptoModels = ArrayList(lCM)
-                        for (crM in cryptoModels!!) {
-                            println(crM.currency)
-                            println(crM.price)
-                        }
-                    }
-                }
-            }
-
-            override fun onFailure(call: Call<List<CryptoModel>>, t: Throwable) {
-                t.printStackTrace()
-            }
-        })
+//        val service = retrofit.create(CryptoAPI::class.java)
+//        val call = service.getData()
+//        call.enqueue(object : Callback<List<CryptoModel>> {
+//            override fun onResponse(
+//                call: Call<List<CryptoModel>>,
+//                response: Response<List<CryptoModel>>
+//            ) {
+//                if (response.isSuccessful) {
+//                    response.body()?.let { lCM ->
+//                        recyclerViewAdapter = RecyclerViewAdapter(lCM as ArrayList<CryptoModel>, this@MainActivity)
+//                        binding.recyclerView.adapter = recyclerViewAdapter
+//
+//                        cryptoModels = ArrayList(lCM)
+//                        for (crM in cryptoModels!!) {
+//                            println(crM.currency)
+//                            println(crM.price)
+//                        }
+//                    }
+//                }
+//            }
+//            override fun onFailure(call: Call<List<CryptoModel>>, t: Throwable) {
+//                t.printStackTrace()
+//            }
+//        })
     }
 
     override fun onItemClick(cryptoModel: CryptoModel) {
         Toast.makeText(this, "Coin: ${cryptoModel.currency}",Toast.LENGTH_LONG).show()
+    }
+
+    private fun handleResponse(cryptoList: List<CryptoModel>) {
+        cryptoModels = ArrayList(cryptoList)
+
+        cryptoModels?.let {
+            recyclerViewAdapter = RecyclerViewAdapter(it, this@MainActivity)
+            binding.recyclerView.adapter = recyclerViewAdapter
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable?.clear()
     }
 }
